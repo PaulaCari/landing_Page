fbb4a87303ecd2341f370333cda7d04b3ba62c11
Paula Cari <paulola0223@gmail.com>
2024-07-06 11:52:39 -0300

    adicione max-width: 1300px;     height: 100%;

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,30 @@
 // console.log("nav");
 const nav_links = document.querySelector(".nav-links");
-const menuBtn_open = open=document.querySelector("menu-btn_open");
-const menuBtn_close = open=document.querySelector("menu-btn_close");
+const menuBtn_open = open=document.querySelector("#menu-btn_open");
+const menuBtn_close = open=document.querySelector("#menu-btn_close");
 // console.log(nav_links, menuBtn_open, menuBtn_close);
 
 menuBtn_open.onclick = () => {
     nav_links.classList.toggle("active");
-
     menuBtn_open.classList.toggle("close");
     menuBtn_close.classList.toggle("open");
 };
 
 menuBtn_close.onclick = () => {
     nav_links.classList.toggle("active");
-
     menuBtn_open.classList.toggle("close");
     menuBtn_close.classList.toggle("open");
 };
 
+// refaturar
+// const nav_links = document.querySelector(".nav-links");
+// const menuBtn_open = document.querySelector("#menu-btn_open");
+// const menuBtn_close = document.querySelector("#menu-btn_close");
+
+// menuBtn_open.onclick = () => toggler();
+// menuBtn_close.onclick = () => toggler();
+// function toggler() {
+//   nav_links.classList.toggle("active");
+//   menuBtn_open.classList.toggle("close");
+//   menuBtn_close.classList.toggle("open");
+// }
